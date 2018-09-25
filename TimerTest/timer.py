9981c5d21a790da8dbee775910a290665dfbc8ae
Bismarck0618 <189916591@qq.com>
2018-09-26 07:53:01 +0800

    定时同步

---
+++ TimerTest/timer.py
@@ -0,0 +1,22 @@
+# enconding = utf-8
+
+import time
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from General.GlobalSetting import conn_k
+from LoadHistoryData.Update_K_Data import update_K_data
+from Test.MACD_Timer import macd_test_daily
+
+
+def MACD_Report():
+    macd_test_daily()b
+
+def update_k():
+    update_K_data()
+    conn_k.commit()
+
+
+sched = BlockingScheduler()
+sched.add_job(func=MACD_Report, trigger='cron', day_of_week='mon-sat', hour=5, minute=30)
+sched.add_job(func=update_k, trigger='cron', day_of_week='mon-sat', hour=6, minute=30)
+sched.start()
