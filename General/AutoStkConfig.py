92e5beaeb8b1604effab68dba7718fa5983147f4
xiaohuilangss <189916591@qq.com>
2019-02-13 19:37:31 +0800

    定时同步

---
+++ General/AutoStkConfig.py
@@ -0,0 +1,10 @@
+# encoding=utf-8
+"""
+这个脚本是用来存储 股票自动检测 程序的配置信息
+
+"""
+
+cubic_test_last_step = 7        # 在曲线拟合时，取最后的几个值进行二次拟合
+
+pic_save_dir_root = 'C:/Users/paul/Desktop/软件代码/Git-Clone/TempPicStore/'
+plot_current_days_amount = 40   # 画出近期的股票走势情况，该参数指示最近取的天数
