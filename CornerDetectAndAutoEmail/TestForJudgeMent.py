8cbbddd4c3e5e0aab259159a2b4b7e0df373f29f
xiaohuilangss <189916591@qq.com>
2019-02-19 07:26:42 +0800

    增加了21天中心均线以及据此判断的拐点

--- CornerDetectAndAutoEmail/TestForJudgeMent.py
+++ CornerDetectAndAutoEmail/TestForJudgeMent.py
@@ -10,7 +10,7 @@ from pylab import *
 """
 本脚本用于测试拐点判断准确性
 """
-sh_index = ts.get_k_data(code='300183',start='2018-01-01')
+sh_index = ts.get_k_data(code='300183', start='2018-01-01')
 
 # 按升序排序
 stk_df = sh_index.sort_values(by='date', ascending=True)
@@ -54,27 +54,51 @@ for idx in stk_df.loc[corner_Pot_Retrospective_Half:len(stk_df)-corner_Pot_Retro
     stk_df.loc[idx, 'err'] = r['err']
 
 
+# 计算收盘价均线，根据均线计算拐点
+stk_df['M21'] = stk_df['close'].rolling(window=21, center=True).mean()
+
+
+# 求解均线拐点
+for idx in stk_df.loc[corner_Pot_Retrospective_Half:len(stk_df)-corner_Pot_Retrospective_Half, :].index:
+
+    # 进行二次曲线拟合
+    r = IsPotInCurveMedian(y_axis=stk_df.loc[idx-corner_Pot_Retrospective_Half:idx+corner_Pot_Retrospective_Half, 'M21'], median_neighbourhood=0.1)
+
+    stk_df.loc[idx, 'corner_flag_M21'] = r['corner_flag']
+    stk_df.loc[idx, 'err_M21'] = r['err']
+
+# 取出秒数轴用于后续的横坐标
+stk_df['second'] = stk_df.apply(lambda x: DateStr2Sec(x['date']), axis=1)
+
 # 画图展示效果
 sh_index = stk_df
-fig, ax = plt.subplots(nrows=2, ncols=1)
+
+fig, ax = plt.subplots(nrows=3, ncols=1)
 
 df_normal = sh_index[sh_index.corner_flag==False]
-x_normal_axis = list(map(lambda x: DateStr2Sec(x), df_normal['date']))
-ax[0].plot(x_normal_axis, df_normal['MACD'],  'g*')
+# x_normal_axis = list(map(lambda x: DateStr2Sec(x), df_normal['date']))
+ax[0].plot(df_normal['second'], df_normal['MACD'],  'g*')
 
-df_corner = sh_index[sh_index.corner_flag_A==True]
-x_corner_axis = list(map(lambda x: DateStr2Sec(x), df_corner['date']))
-ax[0].plot(x_corner_axis, df_corner['MACD'],  'r*')
+df_corner = sh_index[sh_index.corner_flag==True]
+# x_corner_axis = list(map(lambda x: DateStr2Sec(x), df_corner['date']))
+ax[0].plot(df_corner['second'], df_corner['MACD'],  'r*')
 
 # 打印后验拐点
 df_normal_A = sh_index[sh_index.corner_flag_A==False]
-x_normal_axis = list(map(lambda x: DateStr2Sec(x), df_normal_A['date']))
-ax[1].plot(x_normal_axis, df_normal_A['MACD'],  'g*')
+# x_normal_axis = list(map(lambda x: DateStr2Sec(x), df_normal_A['date']))
+ax[1].plot(df_normal_A['second'], df_normal_A['MACD'],  'g*')
 
 
-df_corner_A = sh_index[sh_index.corner_flag==True]
-x_corner_axis = list(map(lambda x: DateStr2Sec(x), df_corner_A['date']))
-ax[1].plot(x_corner_axis, df_corner_A['MACD'],  'r*')
+df_corner_A = sh_index[sh_index.corner_flag_A==True]
+# x_corner_axis = list(map(lambda x: DateStr2Sec(x), df_corner_A['date']))
+ax[1].plot(df_corner_A['second'], df_corner_A['MACD'],  'r*')
+
+
+# 打印中心10均线
+ax[2].plot(sh_index['second'], sh_index['M21'],  'g*')
+M21Corner = sh_index[sh_index['corner_flag_M21']==True]
+ax[2].plot(M21Corner['second'], M21Corner['M21'],  'r*')
+
 
 # plot data
 plt.show()
