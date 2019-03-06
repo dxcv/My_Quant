054221fb03e9fad7c87901c54ffdbdd4f46734ef
paul <1210055099@qq.com>
2019-03-06 16:00:03 +0800

    modify autoconfig parameters，such like stks that we concerned and automatically creat dir that contains the pic send by email！

--- General/AutoStkConfig.py
+++ General/AutoStkConfig.py
@@ -7,10 +7,17 @@ import os
 
 cubic_test_last_step = 7        # 在曲线拟合时，取最后的几个值进行二次拟合
 
+
+# 图片存在的路径，如果没有自动创建
 if os.path.exists('C:/Users/paul/Desktop/软件代码/Git-Clone/TempPicStore/'):
     pic_save_dir_root = 'C:/Users/paul/Desktop/软件代码/Git-Clone/TempPicStore/'
-else:
+
+elif os.path.exists('F:/软件代码/Git-Clone/TempPicStore/'):
     pic_save_dir_root = 'F:/软件代码/Git-Clone/TempPicStore/'
+
+else:
+    os.makedirs('C:/Users/paul/Desktop/软件代码/Git-Clone/TempPicStore/')
+    pic_save_dir_root = 'C:/Users/paul/Desktop/软件代码/Git-Clone/TempPicStore/'
 
 plot_current_days_amount = 40   # 画出近期的stk走势情况，该参数指示最近取的天数
 tailLengthForMacd = 150         # 在计算MACD时，因为之用最近的几个数，所以不需要往前延伸太多，以节省计算量
@@ -20,7 +27,11 @@ stk_list = [
     'cyb',
     'sh',
     'sz',
-    '300183'
+    '300183',
+    '000625',
+    '000725',
+    '000001',
+    '000333'
 ]
 
 step_corner_detect = 6                  # 在判断拐点的时候，取最近的数据的个数
