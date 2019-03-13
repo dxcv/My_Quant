8f025c7bb887d47fc2ff2a310ffcb75840c8aa45
paul <1210055099@qq.com>
2019-03-13 20:39:46 +0800

    定时更新

--- CornerDetectAndAutoEmail/AveMaxMinDetect/Main.py
+++ CornerDetectAndAutoEmail/AveMaxMinDetect/Main.py
@@ -1,22 +1,17 @@
 # encoding=utf-8
-from apscheduler.executors import tornado
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 from CornerDetectAndAutoEmail.AveMaxMinDetect.MaxMin import judgeAndSendMsg, h_l_poy_info, updatePotInfo
 
 
-def trigger():
-    print(str)
-
 sched = BlockingScheduler()
 
-sched.add_job(judgeAndSendMsg,
+sched.add_job(lambda: judgeAndSendMsg(h_l_poy_info),
               'cron',
               day_of_week='mon-fri',
               hour='9-12, 13-23',
               minute='*/2',
-              max_instances=10,
-              kwargs={"df_H_L_Pot": h_l_poy_info})
+              max_instances=10)
 
 
 sched.add_job(updatePotInfo,
@@ -25,4 +20,3 @@ sched.add_job(updatePotInfo,
               second='*/35', misfire_grace_time=3600, coalesce=True)
 
 sched.start()
-tornado.ioloop.IOLoop.instance().start()
