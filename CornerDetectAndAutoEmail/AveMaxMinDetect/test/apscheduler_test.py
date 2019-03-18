e7da73127a7525eb1271312525c2cc3904078f99
paul <1210055099@qq.com>
2019-03-18 11:46:35 +0800

    commit timely

--- CornerDetectAndAutoEmail/AveMaxMinDetect/test/apscheduler_test.py
+++ CornerDetectAndAutoEmail/AveMaxMinDetect/test/apscheduler_test.py
@@ -1,23 +1,24 @@
 # encoding=utf-8
 import pickle
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+from apscheduler.triggers.combining import OrTrigger
+from apscheduler.triggers.cron import CronTrigger
 from CornerDetectAndAutoEmail.AveMaxMinDetect.Global import h_l_pot_info_url
 from CornerDetectAndAutoEmail.AveMaxMinDetect.MaxMin import judgeAndSendMsg, updatePotInfo
+from SDK.MyTimeOPT import get_current_datetime_str
 
+
+def time_now():
+    print(str(get_current_datetime_str()))
+
+trigger = OrTrigger([
+    CronTrigger(hour='11', minute='1-59/3')
+])
 
 sched = BlockingScheduler()
-sched.add_job(judgeAndSendMsg,
-              'cron',
+sched.add_job(time_now,
+              trigger,
               day_of_week='mon-fri',
-              hour='9-12, 13-15',
-              minute='*/5',
               max_instances=10)
-
-sched.add_job(func=updatePotInfo, trigger='cron', day_of_week='mon-fri', hour=5, minute=30, misfire_grace_time=3600, coalesce=True)
-# sched.add_job(updatePotInfo,
-#               'cron',
-#               max_instances=10,
-#               second='*/35', misfire_grace_time=3600, coalesce=True)
 
 sched.start()
