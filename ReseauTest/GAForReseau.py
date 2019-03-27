0e35553c32553eb3c8b80d2fdff6070d3a432cee
paul <1210055099@qq.com>
2019-03-27 16:58:42 +0800

    add mutiThreading for GA Train! it's worked!

--- ReseauTest/GAForReseau.py
+++ ReseauTest/GAForReseau.py
@@ -4,8 +4,8 @@
 
 利用遗传算法优化网格
 """
-import multiprocessing
-
+# 定义多线程
+import multiprocessing as mp
 from Auto_Report.Auto_Email.Email_SendPdf import dumpPickle, loadPickle
 from GA.GA_Sub import initPop, C_M, convertPOP2Reseau, get_fitness, select
 from ReseauTest.Sub import SingleReseauJudge
@@ -21,7 +21,7 @@ def fitness(reseau, df):
 
     amount_unit = 500
     sh_index = df
-
+    stk_code = '300183'
     record_info = {
         'floor_last': 0,
         'money_remain': 20000,
@@ -46,46 +46,48 @@ def fitness(reseau, df):
     return sh_index.tail(1)['total_money'].values[0]
 
 
-""" ----------------------- 准备训练数据 ------------------------------  """
-stk_code = '300183'
-sh_index = ts.get_k_data(code=stk_code)
+if __name__ == '__main__':
+    """ ----------------------- 准备训练数据 ------------------------------  """
+    stk_code = '300183'
+    sh_index = ts.get_k_data(code=stk_code)
 
-sh_index['M20'] = sh_index['close'].rolling(window=20, center=False).mean()
-sh_index['C-M20'] = sh_index.apply(lambda x: x['close']-x['M20'], axis=1)
+    sh_index['M20'] = sh_index['close'].rolling(window=20, center=False).mean()
+    sh_index['C-M20'] = sh_index.apply(lambda x: x['close']-x['M20'], axis=1)
 
-sh_index = sh_index.dropna(how='any')
+    sh_index = sh_index.dropna(how='any')
 
-""" --------------------- 定义 ------------------------ """
-r_bit_len = 7           # 网格二进制长度
-r_amount = 6            # 网格数量
+    """ --------------------- 定义 ------------------------ """
+    r_bit_len = 7           # 网格二进制长度
+    r_amount = 6            # 网格数量
 
-DNA_SIZE = r_bit_len*r_amount                                               # DNA 长度
-POP_SIZE = 100                                                              # 种群大小
-CROSS_RATE = 0.8                                                            # mating probability (DNA crossover)
-MUTATION_RATE = 0.003                                                       # mutation probability
-N_GENERATIONS = 200                                                         # 种群代数
-X_BOUND = [np.min(sh_index['C-M20'])-2, np.max(sh_index['C-M20'])+2]        # 基因的上下限（此处应该设置为“相对price”的上下限）
+    DNA_SIZE = r_bit_len*r_amount                                               # DNA 长度
+    POP_SIZE = 100                                                              # 种群大小
+    CROSS_RATE = 0.8                                                            # mating probability (DNA crossover)
+    MUTATION_RATE = 0.003                                                       # mutation probability
+    N_GENERATIONS = 200                                                         # 种群代数
+    X_BOUND = [np.min(sh_index['C-M20'])-2, np.max(sh_index['C-M20'])+2]        # 基因的上下限（此处应该设置为“相对price”的上下限）
 
-# 初始化种群
-if os.path.exists('./pop_store/pop'+stk_code):
-    pop = loadP('./pop_store/', 'pop'+stk_code)
-else:
-    pop = initPop(POP_SIZE, r_bit_len*r_amount)
-last_fitness = []
-result_money = []
-pop_int = []
+    # 初始化种群
+    if os.path.exists('./pop_store/pop'+stk_code):
+        pop = loadP('./pop_store/', 'pop'+stk_code)
+    else:
+        pop = initPop(POP_SIZE, r_bit_len*r_amount)
+    last_fitness = []
+    result_money = []
+    pop_int = []
 
+    pool = mp.Pool(processes=7)                    # 默认是有几个核就用几个，可以自己设置processes = ？
 
-
-
-def train():
     for i in range(200):
 
         # 基因转为网格
         pop_int = convertPOP2Reseau(pop=pop, reseau_BIT_size=r_bit_len, reseau_amount=r_amount, reseau_bound=X_BOUND)
 
+        fitness_input = list(map(lambda x: (x, sh_index), pop_int))
+
         # 计算种群的适应度
-        pop_fitness = list(map(lambda x: fitness(x, sh_index), pop_int))
+        pop_fitness = pool.starmap(fitness, fitness_input)  # 可以放入可迭代对象，自动分配进程
+        # pop_fitness = list(map(lambda x: fitness(x, sh_index), pop_int))
 
         # 调整适应度
         last_fitness = get_fitness(pop_fitness)
@@ -100,16 +102,11 @@ def train():
 
         result_money.append(np.max(pop_fitness))
 
-# 定义多线程
-pool = multiprocessing.Pool(processes=4)
+    # 训练结束，找出最好的
+    print('收益提高情况：'+str(result_money))
+    print('最终的网格：' + str(pop_int[list(last_fitness).index(np.max(last_fitness))]))
 
-
-# 训练结束，找出最好的
-print('收益提高情况：'+str(result_money))
-print('最终的网格：' + str(pop_int[list(last_fitness).index(np.max(last_fitness))]))
-
-
-dumpP(pop, './pop_store/', 'pop'+stk_code)
-dumpP(pop_int[list(last_fitness).index(np.max(last_fitness))], './pop_store/', 'reseau'+stk_code)
+    dumpP(pop, './pop_store/', 'pop'+stk_code)
+    dumpP(pop_int[list(last_fitness).index(np.max(last_fitness))], './pop_store/', 'reseau'+stk_code)
 
 
