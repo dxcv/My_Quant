26c87cf541ca5d61fc489728dd0e7163b4aa1723
paul <1210055099@qq.com>
2019-05-23 18:19:42 +0800

    timely commit

--- JQData_Test/JQ_Industry_Analysis.py
+++ JQData_Test/JQ_Industry_Analysis.py
@@ -23,11 +23,9 @@ for dic in indus_dic:
     plot(dic['indus_name'], dic['net_profit_list'], year_list)
 
 
-
 # 获取行业中的龙头股，以净利润为指标
-df_list = get_indus_stk_df(jqdatasdk.get_industry_stocks(industry_code='801180'),2010)
-df_list.sort_values(by='net_profit',ascending=False).head(3).loc[:,'code'].values
-
+df_list = get_indus_stk_df(jqdatasdk.get_industry_stocks(industry_code='801180'), 2010)
+df_list.sort_values(by='net_profit', ascending=False).head(3).loc[:, 'code'].values
 
 # 获取房地产的三大龙头与沪深300指数的close数据
 df = jqdatasdk.get_price(['000002.XSHE', '600048.XSHG', '000069.XSHE','000300.XSHG'], start_date='2010-01-01', end_date='2018-10-01', frequency='daily')
@@ -35,12 +33,12 @@ df = jqdatasdk.get_price(['000002.XSHE', '600048.XSHG', '000069.XSHE','000300.XS
 df_close = df['close']
 
 # 沪深300年初10年年初买进3000块，其收益走势如下
-df_close['000300.XSHG_std'] = df_close.apply(lambda x:x['000300.XSHG']*(3000/3535.2290), axis=1)
+df_close['000300.XSHG_std'] = df_close.apply(lambda x: x['000300.XSHG']*(3000/3535.2290), axis=1)
 
 
 # 每支stk起始投资1000块
 for stk in ['000002.XSHE', '600048.XSHG', '000069.XSHE']:
-    df_close[stk+'_std'] = df_close.apply(lambda x:x[stk]*(1000/df_close.head(1)[stk].values[0]), axis=1)
+    df_close[stk+'_std'] = df_close.apply(lambda x: x[stk]*(1000/df_close.head(1)[stk].values[0]), axis=1)
 
 
 # 求取个股收益情况之和
